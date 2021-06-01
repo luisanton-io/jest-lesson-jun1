0ad1ccee3c4c5df9f34d90d797b0d5ef2a67799f
Luis Antonio Canettoli <hello@luisanton.io>
2021-06-01 12:12:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose"
 import server from "./server.js"
 
+const port = process.env.PORT || 3000
+
 mongoose
     .connect(process.env.ATLAS_URL + "prd", { useNewUrlParser: true })
     .then(() => {
         console.log("Connected to Atlas!")
-        server.listen(3000, () => {
+        server.listen(port, () => {
             console.log("Server listening on port 3000")
         })
     })
