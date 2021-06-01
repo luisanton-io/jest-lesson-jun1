c3430561f884495b9be3fe26b23942cfb17f8f79
Carlo Lombardi <carlo_lombardi@users.noreply.github.com>
2021-06-01 14:45:09 +0100

    first push

--- test/index.test.js
+++ test/index.test.js
@@ -1,108 +1,106 @@
-import dotenv from "dotenv"
-import supertest from "supertest"
-import server from "../src/server"
-import mongoose from "mongoose"
-import ProductModel from "../src/models/products/index.js"
-dotenv.config()
+import dotenv from "dotenv";
+import supertest from "supertest";
+import server from "../src/server";
+import mongoose from "mongoose";
+import ProductModel from "../src/models/products/index.js";
+dotenv.config();
 
-const request = supertest(server)
+const request = supertest(server);
 
-describe("Stage I - Testing the test env", () => {
+// describe("Stage I - Testing the test env", () => {
 
-    it("should test that true is true", () => {
-        expect(true).toBe(true)
-    })
+//     it("should test that true is true", () => {
+//         expect(true).toBe(true)
+//     })
 
-    it("should test that false is not true", () => {
-        expect(false).not.toBe(true)
-    })
+//     it("should test that false is not true", () => {
+//         expect(false).not.toBe(true)
+//     })
 
-    it("should test that false is falsy", () => {
-        expect(false).toBeFalsy()
-    })
+//     it("should test that false is falsy", () => {
+//         expect(false).toBeFalsy()
+//     })
 
-    it("should expect that the test key is 123", () => {
-        console.log(process.env.TEST_KEY)
-        expect(process.env.TEST_KEY).toBeDefined()
-        expect(process.env.TEST_KEY).toBe("123")
-    })
+//     it("should expect that the test key is 123", () => {
+//         console.log(process.env.TEST_KEY)
+//         expect(process.env.TEST_KEY).toBeDefined()
+//         expect(process.env.TEST_KEY).toBe("123")
+//     })
 
-
-})
+// })
 
 describe("Checking application main endpoints", () => {
-    it("should check that the /test endpoint is working", async () => {
-        const response = await request.get("/test")
+  it("should check that the /test endpoint is working", async () => {
+    const response = await request.get("/test");
 
-        expect(response.status).toBe(200)
-        expect(response.body.message).toBe("Test success!")
-    })
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe("Test success!");
+  });
 
-    it("should check that the /products endpoint is working", async () => {
-        const response = await request.get("/products")
-        expect(response.status).toBe(200)
-        expect(response.body.products).toBeDefined()
-        expect(response.body.products.length).toBe(0)
-    })
+  it("should check that the /products endpoint is working", async () => {
+    const response = await request.get("/products");
+    expect(response.status).toBe(200);
+    expect(response.body.products).toBeDefined();
+    expect(response.body.products.length).toBe(0);
+  });
 
-    const validData = {
-        description: "Test product",
-        price: 30
-    }
+  const validData = {
+    description: "Test product",
+    price: 30,
+  };
 
-    it("should check that the /products endpoint is allowing POST requests with valid data", async () => {
-        const response = await request.post("/products").send(validData)
-        expect(response.status).toBe(201)
-        expect(response.body._id).toBeDefined()
-        expect(response.body.description).toEqual(validData.description)
-    })
+  it("should check that the /products endpoint is allowing POST requests with valid data", async () => {
+    const response = await request.post("/products").send(validData);
+    expect(response.status).toBe(201);
+    expect(response.body._id).toBeDefined();
+    expect(response.body.description).toEqual(validData.description);
+  });
 
-    const invalidData = {
-        description: "Test product"
-    }
+  const invalidData = {
+    description: "Test product",
+  };
 
-    it("should check that the /products endpoint is NOT allowing POST requests with invalid data", async () => {
-        const response = await request.post("/products").send(invalidData)
-        expect(response.status).toBe(400)
-        expect(response.body._id).not.toBeDefined()
-    })
+  it("should check that the /products endpoint is NOT allowing POST requests with invalid data", async () => {
+    const response = await request.post("/products").send(invalidData);
+    expect(response.status).toBe(400);
+    expect(response.body._id).not.toBeDefined();
+  });
 
-    it("should test that the /products endpoint is returning valid data after creating", async () => {
-        const response = await request.post("/products").send(validData)
+  it("should test that the /products endpoint is returning valid data after creating", async () => {
+    const response = await request.post("/products").send(validData);
 
-        expect(response.body._id).toBeDefined()
+    expect(response.body._id).toBeDefined();
 
-        const product = await ProductModel.findById(response.body._id)
+    const product = await ProductModel.findById(response.body._id);
 
-        expect(product.createdAt).toStrictEqual(new Date(response.body.createdAt))
+    expect(product.createdAt).toStrictEqual(new Date(response.body.createdAt));
+  });
 
-    })
+  it("should test that the /products endpoint is returning all the products available", async () => {
+    const productResponse = await request.post("/products").send(validData);
 
-    it("should test that the /products endpoint is returning all the products available", async () => {
-        const productResponse = await request.post("/products").send(validData)
+    const response = await request.get("/products");
 
-        const response = await request.get("/products")
+    const included = response.body.products.some(
+      (product) => product._id === productResponse.body._id
+    );
 
-        const included = response.body.products.some(product => product._id === productResponse.body._id)
-
-        expect(included).toBe(true)
-
-    })
-
-})
+    expect(included).toBe(true);
+  });
+});
 
 beforeAll((done) => {
-    console.log(process.env.ATLAS_URL)
-    mongoose
-        .connect(process.env.ATLAS_URL + "test", { useNewUrlParser: true })
-        .then(() => {
-            console.log("Successfully connected to Atlas in test.")
-            done()
-        })
-})
+  console.log(process.env.ATLAS_URL);
+  mongoose
+    .connect(process.env.ATLAS_URL + "test", { useNewUrlParser: true })
+    .then(() => {
+      console.log("Successfully connected to Atlas in test.");
+      done();
+    });
+});
 
-afterAll(done => {
-    mongoose.connection.dropDatabase(() => {
-        mongoose.connection.close(() => done())
-    })
-})
+afterAll((done) => {
+  mongoose.connection.dropDatabase(() => {
+    mongoose.connection.close(() => done());
+  });
+});
