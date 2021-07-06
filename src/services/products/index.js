46535ce3f6aed52d4e1aede92710967c21c00d54
baxacsharp <78537820+baxacsharp@users.noreply.github.com>
2021-07-06 15:30:01 +0200

    checking jest working

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -6,25 +6,34 @@ const { Router } = express
 const productsRouter = new Router()
 
 productsRouter.get("/", async (req, res) => {
-    const products = await ProductModel.find({})
-    res.status(200).send({ products })
+  const products = await ProductModel.find({})
+  res.status(200).send({ products })
 })
-
+productsRouter.get("/:id", async (req, res) => {
+  const products = await ProductModel.findById({ _id: req.params.id })
+  if (products) {
+    res.send(products)
+  } else {
+    res.status(404).send({ message: error.message })
+  }
+})
+productsRouter.delete("/:id", async (req, res) => {
+  const products = await ProductModel.findByIdAndDelete({ _id: req.params.id })
+  res.status(204).send("succesfully deleted")
+})
 productsRouter.post("/", async (req, res) => {
+  try {
+    const { description, price } = req.body
 
-    try {
-        const { description, price } = req.body
+    if (!description || !price) throw new Error("Invalid data")
 
-        if (!description || !price) throw new Error("Invalid data")
+    const product = new ProductModel({ description, price })
+    await product.save()
 
-        const product = new ProductModel({ description, price })
-        await product.save()
-
-        res.status(201).send(product)
-
-    } catch (error) {
-        res.status(400).send({ message: error.message })
-    }
+    res.status(201).send(product)
+  } catch (error) {
+    res.status(400).send({ message: error.message })
+  }
 })
 
 export default productsRouter
